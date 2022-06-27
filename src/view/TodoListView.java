4856cda124a514417f0f8197dabc0126497cd2ad
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-06-27 12:55:02 +0700

    View Menghapus TodoList

--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -48,7 +48,15 @@ public class TodoListView {
     }
 
     public void removeTodoList() {
+        System.out.println("MENGHAPUS TODOLIST");
 
+        var number = InputUtil.input("Nomor yang dihapus (x jika batal)");
+
+        if(number.equals("x")){
+            //batal
+        }else {
+            todoListService.removeTodoList(Integer.valueOf(number));
+        }
     }
 
 }
