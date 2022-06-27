dd615057191756aabed6ef25739ea3c653a08aa4
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-06-27 11:28:13 +0700

    Test Repository & Service Menghapus TodoList

--- src/test/service/TodoListServiceTest.java
+++ src/test/service/TodoListServiceTest.java
@@ -11,7 +11,8 @@ public class TodoListServiceTest {
     public static void main(String[] args) {
 
         //testShowTodoList();
-        testAddTodoList();
+        //testAddTodoList();
+        testRemoveTodoList();
     }
 
     public static void testShowTodoList() {
@@ -33,6 +34,27 @@ public class TodoListServiceTest {
         todoListService.addTodoList("Belajar Java OOP");
         todoListService.addTodoList("Belajar Java Standard Classes");
 
+        todoListService.showTodoList();
+    }
+
+    public static void testRemoveTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Belajar Java Standard Classes");
+
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(5);
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+        todoListService.removeTodoList(1);
         todoListService.showTodoList();
     }
 
